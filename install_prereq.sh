43fd8acae5114a07bec2c2c03c0fa647976e6792
Andrijan Möcker <36304504+ctandi@users.noreply.github.com>
2019-01-18 15:10:05 +0100

    Remove HTTP
    Already included in Python3 and not required for the Python2 scripts.

--- install_prereq.sh
+++ install_prereq.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y dnsmasq hostapd screen curl python-pip python-setuptools python-wheel mosquitto nodejs
 
-sudo pip install paho-mqtt pyaes tornado http
+sudo pip install paho-mqtt pyaes tornado
 
 pushd scripts/smartconfig
 npm i
