b9aa69eb8d54dad942b69791aa1f0d1cf11885c0
Merlin Schumacher <merlin.schumacher@gmail.com>
2019-01-21 17:39:30 +0100

    Update start_flash.sh

--- start_flash.sh
+++ start_flash.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+bold=$(tput bold)
+normal=$(tput sgr0)
 . ./config.txt
 
 ./stop_flash.sh >/dev/null
@@ -7,16 +8,19 @@
 pushd scripts
 
 echo "======================================================"
-echo "VTRUST-TUYA-CONVERT"
+echo "${bold}TUYA-CONVERT${normal}"
+echo
 echo "https://github.com/vtrust-de/smarthome-smarthack"
-echo "VTRUST-TUYA-CONVERT was developed by Michael Steigerwald from the IT security company VTRUST in collaboration with the techjournalists Merlin Schumacher, Pina Merkert, Andrijan Moecker and Jan Mahn at ct magazine."
+echo "TUYA-CONVERT was developed by Michael Steigerwald from the IT security company VTRUST (https://www.vtrust.de/) in collaboration with the techjournalists Merlin Schumacher, Pina Merkert, Andrijan Moecker and Jan Mahn at c't Magazine. (https://www.ct.de/)"
+echo 
+echo 
 echo "======================================================"
-echo "PLEASE READ THIS CAREFULLY!"
+echo "${bold}PLEASE READ THIS CAREFULLY!${normal}"
 echo "======================================================"
-echo "VTRUST-TUYA-CONVERT creates a fake update server environment for ESP8266/85 based tuya devices. It enables you to backup your devices firmware and upload an alternative one (e.g. ESPEasy, Tasmota, Espurna) without the need to open the device and solder a serial connection (OTA, Over-the-air)."
+echo "TUYA-CONVERT creates a fake update server environment for ESP8266/85 based tuya devices. It enables you to backup your devices firmware and upload an alternative one (e.g. ESPEasy, Tasmota, Espurna) without the need to open the device and solder a serial connection (OTA, Over-the-air)."
 echo "Please make sure that you understand the consequences of flashing an alternative firmware, since you might lose functionality!"
 echo
-echo "Flashing an alternative firmware can cause unexpected device behavior and/or render the device unusable. Be aware that you do use this software at YOUR OWN RISK! Please acknowledge that VTRUST and ct (heise) CAN NOT be held accountable for ANY DAMAGE or LOSS OF FUNCTIONALITY by typing yes + Enter"
+echo "Flashing an alternative firmware can cause unexpected device behavior and/or render the device unusable. Be aware that you do use this software at YOUR OWN RISK! Please acknowledge that VTRUST and ct (heise) CAN NOT be held accountable for ANY DAMAGE or LOSS OF FUNCTIONALITY by typing ${bold}yes + Enter${normal}"
 echo 
 read
 if [ "$REPLY" != "yes" ]; then
@@ -29,20 +33,20 @@ echo "  Stopping any Webserver"
 sudo service apache2 stop >/dev/null 2>&1
 echo "  Starting Websever in a screen"
 sudo screen -S smarthack-web -m -d ./fake-registration-server.py
-service mosquitto stop >/dev/null 2>&1
 echo "  Starting Mosquitto in a screen"
+sudo service mosquitto stop >/dev/null 2>&1
 sudo screen -S smarthack-mqtt -m -d mosquitto -v
 echo
 echo "======================================================"
 echo
 echo "IMPORTANT"
 echo "1. Connect any another device (a smartphone or something) to the WIFI $AP"
-echo "   The wpa-password is $PASS"
+echo "   The wpa-password is ${bold}$PASS${normal}"
 echo "   This step is IMPORTANT otherwise the smartconfig will not work!"
 echo "2. Put your IoT device in autoconfig/smartconfig/paring mode (LED will blink fast). This is usually done by pressing and holding the primary button of the device"
-echo "3. Press ENTER to continue"
+echo "3. Press ${bold}ENTER${normal} to continue"
 read x
-echo
+echo ""
 echo "======================================================"
 echo "Starting pairing procedure in screen"
 sudo ip route add 255.255.255.255 dev $WLAN
