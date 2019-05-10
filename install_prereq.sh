d50d08a785bc18323781b0c8432426c666aa1c58
DoktorHeisenberg <michael@friebe.it>
2019-05-10 18:58:37 +0200

    Update install_prereq.sh

--- install_prereq.sh
+++ install_prereq.sh
@@ -4,7 +4,7 @@ set -e
 
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y dnsmasq hostapd screen curl python3-pip python3-setuptools python3-wheel mosquitto haveged
+sudo apt-get install -y dnsmasq hostapd screen curl python3-pip python3-setuptools python3-wheel mosquitto haveged net-tools
 
 sudo pip3 install paho-mqtt pyaes tornado
 
