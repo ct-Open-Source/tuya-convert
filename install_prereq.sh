ed4fcaf8a482c0910d1bd3a777701c81017df4ac
Madmartigan <55095476+M4dmartig4n@users.noreply.github.com>
2019-09-18 22:15:27 +0200

    fix typo

--- install_prereq.sh
+++ install_prereq.sh
@@ -6,6 +6,6 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y dnsmasq hostapd screen curl python3-pip python3-setuptools python3-wheel mosquitto haveged net-tools
 
-sudo pip3 install paho-mqtt pyaes tornado git+https://github.com/M4dmartig4n/sslpsk.gi
+sudo pip3 install paho-mqtt pyaes tornado git+https://github.com/M4dmartig4n/sslpsk.git
 
 echo "Ready to start upgrade"
