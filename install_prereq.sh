2903bd286e2aad72014e3351dbca26e3803ea11c
Colin Kuebler <kueblc@gmail.com>
2019-09-21 16:06:16 -0400

    Added python3-dev and pycrypto as per #274

--- install_prereq.sh
+++ install_prereq.sh
@@ -4,8 +4,7 @@ set -e
 
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y dnsmasq hostapd screen curl python3-pip python3-setuptools python3-wheel mosquitto haveged net-tools
-
-sudo pip3 install paho-mqtt pyaes tornado git+https://github.com/M4dmartig4n/sslpsk.git
+sudo apt-get install -y dnsmasq hostapd screen curl python3-pip python3-setuptools python3-wheel python3-dev mosquitto haveged net-tools
+sudo pip3 install paho-mqtt pyaes tornado git+https://github.com/M4dmartig4n/sslpsk.git pycrypto
 
 echo "Ready to start upgrade"
