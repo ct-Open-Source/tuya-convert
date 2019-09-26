4195c53daf9bebc730542d38e83841ca39f93f1a
Colin Kuebler <kueblc@gmail.com>
2019-09-26 13:55:01 -0400

    Experimental workaround, some users are reporting better luck with python vs python3 for psk-frontend

--- install_prereq.sh
+++ install_prereq.sh
@@ -5,6 +5,7 @@ set -e
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y dnsmasq hostapd screen curl python3-pip python3-setuptools python3-wheel python3-dev mosquitto haveged net-tools libssl-dev
-sudo pip3 install paho-mqtt pyaes tornado git+https://github.com/M4dmartig4n/sslpsk.git pycrypto
+sudo -H pip3 install paho-mqtt pyaes tornado git+https://github.com/M4dmartig4n/sslpsk.git pycrypto
+sudo -H pip install git+https://github.com/M4dmartig4n/sslpsk.git pycrypto
 
 echo "Ready to start upgrade"
