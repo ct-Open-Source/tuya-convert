153c8ed84253c56cff4f62a319c6345a10940dff
Colin Kuebler <kueblc@gmail.com>
2019-09-26 12:47:39 -0400

    Start dnsmasq with command line options rather than a config file No need to backup or restore dnsmasq.conf Use wildcard to resolve all dns queries to 10.42.42.1 Write hostapd.conf locally so we don't need to backup/restore Use sudo when writing NetworkManager.conf Added and tweaked some log messages Use pkill to stop hostapd so the cleanup portion of setup_ap is run

--- scripts/setup_ap.sh
+++ scripts/setup_ap.sh
@@ -20,51 +20,24 @@ if [ -n "$wpa_supplicant_pid" ]; then
 fi
 
 if test -d /etc/NetworkManager; then
-	echo "Backing up NetworkManager.cfg..."
+	echo "Backing up NetworkManager.conf..."
 	sudo cp -n /etc/NetworkManager/NetworkManager.conf /etc/NetworkManager/NetworkManager.conf.backup
 
-	cat <<- EOF > /etc/NetworkManager/NetworkManager.conf
+	sudo bash -c 'cat <<- EOF > /etc/NetworkManager/NetworkManager.conf
 		[main]
 		plugins=keyfile
 
 		[keyfile]
 		unmanaged-devices=interface-name:$WLAN
-	EOF
+	EOF'
 
 	echo "Restarting NetworkManager..."
 	sudo service network-manager restart
 fi
 sudo ifconfig $WLAN up
 
-echo "Backing up /etc/dnsmasq.conf..."
-sudo cp -n /etc/dnsmasq.conf /etc/dnsmasq.conf.backup
-
-
-echo "Writing dnsmasq config file..."
-echo "Creating new /etc/dnsmasq.conf..."
-cat <<- EOF >/etc/dnsmasq.conf
-	# disables dnsmasq reading any other files like /etc/resolv.conf for nameservers
-	no-resolv
-	# Interface to bind to
-	interface=$WLAN
-	bind-interfaces
-	listen-address=10.42.42.1
-	#Specify starting_range,end_range,lease_time
-	dhcp-range=10.42.42.10,10.42.42.40,12h
-	# dns addresses to send to the clients
-	server=9.9.9.9
-	server=1.1.1.1
-	address=/tuya.com/10.42.42.1
-	address=/tuyaeu.com/10.42.42.1
-	address=/tuyaus.com/10.42.42.1
-	address=/tuyacn.com/10.42.42.1
-EOF
-
-echo "Backing up /etc/hostapd/hostapd.conf..."
-sudo cp -n /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.backup
-
 echo "Writing hostapd config file..."
-cat <<- EOF >/etc/hostapd/hostapd.conf
+cat <<- EOF >hostapd.conf
 	interface=$WLAN
 	driver=nl80211
 	ssid=$AP
@@ -91,27 +64,32 @@ sudo iptables --table nat --append POSTROUTING --out-interface $ETH -j MASQUERAD
 sudo iptables --append FORWARD --in-interface $WLAN -j ACCEPT
 
 echo "Starting DNSMASQ server..."
-sudo /etc/init.d/dnsmasq stop > /dev/null 2>&1
-sudo pkill dnsmasq
-sudo dnsmasq
+sudo dnsmasq \
+	--no-resolv \
+	--interface=$WLAN \
+	--bind-interfaces \
+	--listen-address=10.42.42.1 \
+	--dhcp-range=10.42.42.10,10.42.42.40,12h \
+	--server=9.9.9.9 \
+	--server=1.1.1.1 \
+	--address=/#/10.42.42.1
 
 sudo sysctl -w net.ipv4.ip_forward=1 > /dev/null 2>&1
 
 sudo ip route add 255.255.255.255 dev $WLAN
 
 
-echo "Starting AP on $WLAN in screen terminal..."
-sudo hostapd /etc/hostapd/hostapd.conf
+echo "Starting AP on $WLAN..."
+sudo hostapd hostapd.conf
+echo "AP closed"
 
 if test -d /etc/NetworkManager; then
+	echo "Restoring NetworkManager.conf..."
 	sudo mv /etc/NetworkManager/NetworkManager.conf.backup /etc/NetworkManager/NetworkManager.conf
 	sudo service network-manager restart
 fi
-sudo /etc/init.d/dnsmasq stop > /dev/null 2>&1
+echo "Stopping DNSMASQ server..."
 sudo pkill dnsmasq
-sudo mv /etc/dnsmasq.conf.backup /etc/dnsmasq.conf > /dev/null 2>&1
-sudo rm /etc/dnsmasq.hosts > /dev/null 2>&1
-sudo mv /etc/hostapd/hostapd.conf.backup /etc/hostapd/hostapd.conf > /dev/null 2>&1
 sudo iptables --flush
 sudo iptables --flush -t nat
 sudo iptables --delete-chain
--- stop_flash.sh
+++ stop_flash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Stopping AP in a screen"
-sudo screen -S smarthack-wifi         -X stuff '^C'
+sudo pkill hostapd
 sudo screen -S smarthack-web          -X stuff '^C'
 sudo screen -S smarthack-smartconfig  -X stuff '^C'
 sudo screen -S smarthack-mqtt         -X stuff '^C'
