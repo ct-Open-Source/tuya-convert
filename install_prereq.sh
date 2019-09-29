85f4bd3fa26a819dd69f78b162a44bf6b6ff7846
Colin Kuebler <kueblc@gmail.com>
2019-09-29 12:34:29 -0400

    Added python-setuptools for python-pip

--- install_prereq.sh
+++ install_prereq.sh
@@ -4,7 +4,7 @@ set -e
 
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y dnsmasq hostapd screen curl python-pip python3-pip python3-setuptools python3-wheel python3-dev mosquitto haveged net-tools libssl-dev
+sudo apt-get install -y dnsmasq hostapd screen curl python-pip python3-pip python-setuptools python3-setuptools python3-wheel python3-dev mosquitto haveged net-tools libssl-dev
 sudo -H pip3 install paho-mqtt pyaes tornado git+https://github.com/M4dmartig4n/sslpsk.git pycrypto
 sudo -H pip2 install git+https://github.com/M4dmartig4n/sslpsk.git pycrypto
 
