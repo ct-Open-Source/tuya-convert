e1126b87b7509793eebe078f42b0eac8f7551d45
Colin Kuebler <kueblc@gmail.com>
2019-10-03 13:24:55 -0400

    Closing AP should be the last thing we do, as the other scripts are bound to this interface

--- stop_flash.sh
+++ stop_flash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Stopping AP in a screen"
-sudo pkill hostapd
 sudo screen -S smarthack-web          -X stuff '^C'
 sudo screen -S smarthack-smartconfig  -X stuff '^C'
 sudo screen -S smarthack-mqtt         -X stuff '^C'
+echo "Closing AP"
+sudo pkill hostapd
