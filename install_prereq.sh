c7683d82243a1278339234770afa6a77a819d458
Erwan MAS <erwan@mas.nom.fr>
2019-10-05 19:42:08 +0000

    for debian buster aka 10.X we install python-wheel ( for python 2.7 )

--- install_prereq.sh
+++ install_prereq.sh
@@ -5,6 +5,10 @@ set -e
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y dnsmasq hostapd screen curl python-pip python3-pip python-setuptools python3-setuptools python3-wheel python-dev python3-dev mosquitto haveged net-tools libssl-dev
+if [ $(grep -c '^10\.' /etc/debian_version 2>/dev/null ) = 1 ]
+then
+     apt-get install python-wheel
+fi
 sudo -H pip3 install paho-mqtt tornado git+https://github.com/M4dmartig4n/sslpsk.git pycrypto
 sudo -H pip2 install git+https://github.com/M4dmartig4n/sslpsk.git pycrypto
 
