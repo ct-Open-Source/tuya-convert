dbd6991c9d606f806a89307da1e4f195919e581b
Colin Kuebler <kueblc@gmail.com>
2020-01-01 18:44:02 -0500

    Use python3 to install dependencies to ensure they are installed in the correct version on setups with more than one python

--- install_prereq.sh
+++ install_prereq.sh
@@ -5,13 +5,6 @@ set -e
 sudo apt-get update
 sudo apt-get install -y git iw dnsmasq hostapd screen curl build-essential python3-pip python3-setuptools python3-wheel python3-dev mosquitto haveged net-tools libssl-dev
 
-PY_DEPENDENCIES="paho-mqtt tornado git+https://github.com/drbild/sslpsk.git@use-byte-string-for-identity-hints pycryptodomex"
-
-if python3 -c 'import sys; exit(0) if sys.version_info.major == 3 and sys.version_info.minor < 7 else exit(1)' ;
-then
-	sudo -H pip3 install $PY_DEPENDENCIES
-else
-	sudo -H python3 -m pip install $PY_DEPENDENCIES
-fi
+sudo -H python3 -m pip install paho-mqtt tornado git+https://github.com/drbild/sslpsk.git@use-byte-string-for-identity-hints pycryptodomex
 
 echo "Ready to start upgrade"
