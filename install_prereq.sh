50a03c83cb9461232d0718952fa485cf601adf3b
Colin Kuebler <kueblc@gmail.com>
2020-01-24 15:25:31 -0500

    Use upgrade flag in pip to force sslpsk reinstall, fix #497, fix #508 thanks @Lovely-Maisonette

--- install_prereq.sh
+++ install_prereq.sh
@@ -5,6 +5,6 @@ set -e
 sudo apt-get update
 sudo apt-get install -y git iw dnsmasq hostapd screen curl build-essential python3-pip python3-setuptools python3-wheel python3-dev mosquitto haveged net-tools libssl-dev
 
-sudo -H python3 -m pip install paho-mqtt tornado git+https://github.com/drbild/sslpsk.git@use-byte-string-for-identity-hints pycryptodomex
+sudo -H python3 -m pip install --upgrade paho-mqtt tornado git+https://github.com/drbild/sslpsk.git@use-byte-string-for-identity-hints pycryptodomex
 
 echo "Ready to start upgrade"
