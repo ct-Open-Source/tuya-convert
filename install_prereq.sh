c628c4717c9d301ff0de08120428b3de2e10c6a8
Jeff Rescignano <jeff@jeffresc.dev>
2020-05-15 02:42:26 -0400

    Programically download the latest update of tasmota during the install_prereq.sh script

--- install_prereq.sh
+++ install_prereq.sh
@@ -7,6 +7,7 @@ sudo apt-get install -y git iw dnsmasq hostapd screen curl build-essential pytho
 
 latest_espurna_version=$(curl -s 'https://api.github.com/repos/xoseperez/espurna/releases/latest' | grep -oP '"tag_name": "\K(.*)(?=")')
 curl -Lo files/espurna.bin https://github.com/xoseperez/espurna/releases/download/"$latest_espurna_version"/espurna-"$latest_espurna_version"-espurna-base-1MB.bin
+curl -o files/tasmota.bin http://thehackbox.org/tasmota/release/tasmota-lite.bin
 
 sudo -H python3 -m pip install --upgrade paho-mqtt tornado git+https://github.com/drbild/sslpsk.git pycryptodomex
 
