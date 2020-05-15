92e4d7b9adce27fdc5517ed1faace43a69841e35
Jeff Rescignano <jeff@jeffresc.dev>
2020-05-15 02:42:03 -0400

    Programically download the latest update of espurna during the install_prereq.sh script

--- install_prereq.sh
+++ install_prereq.sh
@@ -5,6 +5,9 @@ set -e
 sudo apt-get update
 sudo apt-get install -y git iw dnsmasq hostapd screen curl build-essential python3-pip python3-setuptools python3-wheel python3-dev mosquitto haveged net-tools libssl-dev
 
+latest_espurna_version=$(curl -s 'https://api.github.com/repos/xoseperez/espurna/releases/latest' | grep -oP '"tag_name": "\K(.*)(?=")')
+curl -Lo files/espurna.bin https://github.com/xoseperez/espurna/releases/download/"$latest_espurna_version"/espurna-"$latest_espurna_version"-espurna-base-1MB.bin
+
 sudo -H python3 -m pip install --upgrade paho-mqtt tornado git+https://github.com/drbild/sslpsk.git pycryptodomex
 
 echo "Ready to start upgrade"
