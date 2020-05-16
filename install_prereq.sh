745707882a2d8437cdb14c64b9f62dfd9b8d00c6
clach04 <clach04@gmail.com>
2020-05-15 21:19:09 -0700

    Document why global pip install is used
    Explain why, and discourage anyone from attempting to use virtualenv/venv

--- install_prereq.sh
+++ install_prereq.sh
@@ -5,6 +5,7 @@ set -e
 sudo apt-get update
 sudo apt-get install -y git iw dnsmasq hostapd screen curl build-essential python3-pip python3-setuptools python3-wheel python3-dev mosquitto haveged net-tools libssl-dev
 
+# NOTE a virtualenv/venv will not work due to the way sudo screen is used to start scripts/servers
 sudo -H python3 -m pip install --upgrade paho-mqtt tornado git+https://github.com/drbild/sslpsk.git pycryptodomex
 
 echo "Ready to start upgrade"
