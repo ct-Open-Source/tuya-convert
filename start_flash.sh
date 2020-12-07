da88d9a255de3c0e330b1af11beef2734430c0df
Ryan A. Pavlik <ryan.pavlik@gmail.com>
2020-12-07 12:12:11 -0600

    Update start_flash.sh
    Mention an apparently common case of too-new firmware.
    Took me a while to find my device mentioned on that list,
    so I figured it might be useful.

--- start_flash.sh
+++ start_flash.sh
@@ -82,6 +82,10 @@ while true; do
 			echo "Device did not appear with the intermediate firmware"
 			echo "Check the *.log files in the scripts folder"
 			pkill -f smartconfig/main.py && echo "Stopping smart config"
+			echo "If the device left easy pairing mode (LED stopped blinking quickly) when config"
+			echo "packets were sent, your device may have too new of firmware for this tool: "
+			echo "see https://github.com/ct-Open-Source/tuya-convert/wiki/Collaboration-document-for-PSK-Identity-02#known-affected-devices"
+			echo ""
 			read -p "Do you want to try flashing another device? [y/N] " -n 1 -r
 			echo
 			[[ "$REPLY" =~ ^[Yy]$ ]] || break 2
